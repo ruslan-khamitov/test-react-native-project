3849830bc601d467b261c35ea97da294f09e973a
Khamitov Ruslan <hamitovra@lexema.ru>
2022-08-14 23:16:18 +0500

    feat: Stock screen

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 function About() {
-  return <View />;
+  return (
+    <View style={StyleSheet.absoluteFill}>
+      <Text style={styles.about}>
+        Для создания приложения были использованы: TypeScript, MobX
+      </Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  about: {
+    marginTop: 40,
+    textAlign: 'center',
+  },
+});
 
 export default About;
